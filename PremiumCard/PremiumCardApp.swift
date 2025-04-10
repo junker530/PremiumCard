6a503cb5c2fbbd2007342cf436bc4d63f2d6e68a
junker530 <ddr4646@gmail.com>
2025-04-10 10:15:38 +0900

    Initial Commit

---
+++ PremiumCard/PremiumCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremiumCardApp.swift
+//  PremiumCard
+//
+//  Created by Shota Sakoda on 2025/04/10.
+//
+
+import SwiftUI
+
+@main
+struct PremiumCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PremiumCardTests/PremiumCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  PremiumCardTests.swift
+//  PremiumCardTests
+//
+//  Created by Shota Sakoda on 2025/04/10.
+//
+
+import Testing
+@testable import PremiumCard
+
+struct PremiumCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
